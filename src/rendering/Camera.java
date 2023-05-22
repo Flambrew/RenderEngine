1bb33b0c4f0c8af2258b8b6a0ad7ef8a27e2f156
Flambrew <77941661+Flambrew@users.noreply.github.com>
2023-05-22 19:28:06 -0400

    added lightsource to camera

--- src/rendering/Camera.java
+++ src/rendering/Camera.java
@@ -3,11 +3,12 @@ package src.rendering;
 import src.data.Vector3;
 
 public class Camera {
-	private Vector3 transform, rotation;
+	private Vector3 transform, rotation, lightsource;
 
-	public Camera(Vector3 transform, Vector3 rotation) {
+	public Camera(Vector3 transform, Vector3 rotation, Vector3 lightsource) {
 		this.transform = transform;
 		this.rotation = rotation;
+		this.lightsource = lightsource;
 	}
 
 	public Vector3 transform() {
@@ -16,5 +17,9 @@ public class Camera {
 
 	public Vector3 rotation() {
 		return rotation;
+	}
+
+	public Vector3 lightsource() {
+		return lightsource;
 	}
 }
