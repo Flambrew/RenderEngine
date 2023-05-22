fdd216fa3d57c835c34ff53be476321a9f77b69a
Flambrew <andrew.matherne24@bishopireton.org>
2023-05-22 14:55:42 -0400

    removed framerate from camera

--- src/rendering/Camera.java
+++ src/rendering/Camera.java
@@ -5,17 +5,11 @@ import java.awt.Dimension;
 import src.data.Vector3;
 
 public class Camera {
-	private Dimension windowSize;
 	private Vector3 transform, rotation;
 
-	public Camera(Dimension windowSize, Vector3 transform, Vector3 rotation) {
-		this.windowSize = windowSize;
+	public Camera(Vector3 transform, Vector3 rotation) {
 		this.transform = transform;
 		this.rotation = rotation;
-	}
-
-	public Dimension windowSize() {
-		return windowSize;
 	}
 
 	public Vector3 transform() {
