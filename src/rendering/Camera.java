075e5c33c374f1afc238bed06df4b6ab98241691
Flambrew <andrew.matherne24@bishopireton.org>
2023-05-24 08:44:44 -0400

    removed lightsource from camera - will later implement lighting as another camera.. i think.. we'll see

--- src/rendering/Camera.java
+++ src/rendering/Camera.java
@@ -3,12 +3,11 @@ package src.rendering;
 import src.data.Vector3;
 
 public class Camera {
-	private Vector3 transform, rotation, lightsource;
+	private Vector3 transform, rotation;
 
-	public Camera(Vector3 transform, Vector3 rotation, Vector3 lightsource) {
+	public Camera(Vector3 transform, Vector3 rotation) {
 		this.transform = transform;
 		this.rotation = rotation;
-		this.lightsource = lightsource;
 	}
 
 	public Vector3 transform() {
@@ -17,9 +16,5 @@ public class Camera {
 
 	public Vector3 rotation() {
 		return rotation;
-	}
-
-	public Vector3 lightsource() {
-		return lightsource;
 	}
 }
