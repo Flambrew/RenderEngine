8ec728d96f53ce7e56f3a2e56b5a1e101b11da73
Flambrew <andrew.matherne24@bishopireton.org>
2023-05-24 10:49:15 -0400

    allowed Mesh.translate to take in an array of matrices

--- src/rendering/Mesh.java
+++ src/rendering/Mesh.java
@@ -27,13 +27,13 @@ public class Mesh {
 	public void orderByDistanceFrom(Vector3 point) {
 		triangles.sort(new Comparator<Triangle>() {
 			public int compare(Triangle a, Triangle b) {
-				return Vector3.distanceBetween(point, Vector3.meanVector(a.asArray())) < Vector3.distanceBetween(point, Vector3.meanVector(b.asArray())) ? 1 : -1;
+				return Vector3.distanceBetween(point, Vector3.centroid(a.asArray())) < Vector3.distanceBetween(point, Vector3.centroid(b.asArray())) ? 1 : -1;
 			}
 		});
 	}
 
 	public void paint(Graphics g, FLEngine3D engine, Matrix4 projectionMatrix) {
-		for (Triangle triangle : triangles) 
+		for (Triangle triangle : triangles)
 			triangle.paint(g, engine, projectionMatrix);
 	}
 
@@ -42,38 +42,40 @@ public class Mesh {
 			triangle.a = triangle.a.sum(translation);
 			triangle.b = triangle.b.sum(translation);
 			triangle.c = triangle.c.sum(translation);
-		}	
+		}
 		return this;
 	}
 
-	public Mesh translate(Matrix4 translation) {
-		for (Triangle triangle : triangles) {
-			triangle.a = triangle.a.transform(translation);
-			triangle.b = triangle.b.transform(translation);
-			triangle.c = triangle.c.transform(translation);
-		}	
+	public Mesh translate(Matrix4... translation) {
+		for (Matrix4 matrix : translation) {
+			for (Triangle triangle : triangles) {
+				triangle.a = triangle.a.transform(matrix);
+				triangle.b = triangle.b.transform(matrix);
+				triangle.c = triangle.c.transform(matrix);
+			}
+		}
 		return this;
 	}
 
 	public static Mesh CUBE() {
-		return new Mesh(new Triangle[] {
-			new Triangle(new Vector3(1, 1, 0), new Vector3(1, 1, 1), new Vector3(1, 0, 1)), // x
-			new Triangle(new Vector3(1, 0, 1), new Vector3(1, 0, 0), new Vector3(1, 1, 0)),
+		return new Mesh(new Triangle[] { //
+				new Triangle(new Vector3(1, 1, 0), new Vector3(1, 1, 1), new Vector3(1, 0, 1)), // x
+				new Triangle(new Vector3(1, 0, 1), new Vector3(1, 0, 0), new Vector3(1, 1, 0)), //
 
-			new Triangle(new Vector3(0, 1, 1), new Vector3(0, 1, 0), new Vector3(0, 0, 0)), // -x
-			new Triangle(new Vector3(0, 0, 0), new Vector3(0, 0, 1), new Vector3(0, 1, 1)),
+				new Triangle(new Vector3(0, 1, 1), new Vector3(0, 1, 0), new Vector3(0, 0, 0)), // -x
+				new Triangle(new Vector3(0, 0, 0), new Vector3(0, 0, 1), new Vector3(0, 1, 1)), //
 
-			new Triangle(new Vector3(1, 1, 0), new Vector3(0, 1, 0), new Vector3(0, 1, 1)), // y
-			new Triangle(new Vector3(0, 1, 1), new Vector3(1, 1, 1), new Vector3(1, 1, 0)),
+				new Triangle(new Vector3(1, 1, 0), new Vector3(0, 1, 0), new Vector3(0, 1, 1)), // y
+				new Triangle(new Vector3(0, 1, 1), new Vector3(1, 1, 1), new Vector3(1, 1, 0)), //
 
-			new Triangle(new Vector3(0, 0, 0), new Vector3(1, 0, 0), new Vector3(1, 0, 1)), // -y
-			new Triangle(new Vector3(1, 0, 1), new Vector3(0, 0, 1), new Vector3(0, 0, 0)),
+				new Triangle(new Vector3(0, 0, 0), new Vector3(1, 0, 0), new Vector3(1, 0, 1)), // -y
+				new Triangle(new Vector3(1, 0, 1), new Vector3(0, 0, 1), new Vector3(0, 0, 0)), //
 
-			new Triangle(new Vector3(0, 1, 1), new Vector3(0, 0, 1), new Vector3(1, 0, 1)), // z
-			new Triangle(new Vector3(1, 0, 1), new Vector3(1, 1, 1), new Vector3(0, 1, 1)),
+				new Triangle(new Vector3(0, 1, 1), new Vector3(0, 0, 1), new Vector3(1, 0, 1)), // z
+				new Triangle(new Vector3(1, 0, 1), new Vector3(1, 1, 1), new Vector3(0, 1, 1)), //
 
-			new Triangle(new Vector3(0, 0, 0), new Vector3(0, 1, 0), new Vector3(1, 1, 0)), // -z
-			new Triangle(new Vector3(1, 1, 0), new Vector3(1, 0, 0), new Vector3(0, 0, 0))
+				new Triangle(new Vector3(0, 0, 0), new Vector3(0, 1, 0), new Vector3(1, 1, 0)), // -z
+				new Triangle(new Vector3(1, 1, 0), new Vector3(1, 0, 0), new Vector3(0, 0, 0)) //
 		});
 	};
 }
